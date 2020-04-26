7263467746f8dd4afaa2441100959ff23327dbdd
Yoann Guion <yguion@enovacom.fr>
2020-04-26 22:30:03 +0200

    only one SPEC

--- docker-rpm-build.sh
+++ docker-rpm-build.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 set -e "${VERBOSE:+-x}"
 
-SPEC="${1:?}"
 TOPDIR="${HOME}/rpmbuild"
 
 # copy sources and spec into rpmbuild's work dir
 cp "${VERBOSE:+-v}" -a --reflink=auto * "${TOPDIR}/SOURCES/"
 cp "${VERBOSE:+-v}" -a --reflink=auto "${SPEC}" "${TOPDIR}/SPECS/"
-SPEC="${TOPDIR}/SPECS/${SPEC##*/}"
 
 # build the RPMs
-/srpm-tool-get-sources ${SPEC} "${TOPDIR}/SOURCES/"
-rpmbuild "${VERBOSE:+-v}" -ba "${SPEC}"
+/srpm-tool-get-sources ${TOPDIR}/SPECS/${SPEC}" "${TOPDIR}/SOURCES/"
+rpmbuild "${VERBOSE:+-v}" -ba "${TOPDIR}/SPECS/${SPEC}"
