9057ea9d9836e6e4113d3c70b1150a018133d117
Yoann Guion <yguion@enovacom.fr>
2020-04-26 23:37:02 +0200

    fix typo

--- docker-rpm-build.sh
+++ docker-rpm-build.sh
@@ -7,5 +7,5 @@ cp -a --reflink=auto * "${TOPDIR}/SOURCES/"
 cp -a --reflink=auto "${SPEC}" "${TOPDIR}/SPECS/"
 
 # build the RPMs
-/srpm-tool-get-sources ${TOPDIR}/SPECS/${SPEC}" "${TOPDIR}/SOURCES/"
+/srpm-tool-get-sources "${TOPDIR}/SPECS/${SPEC}" "${TOPDIR}/SOURCES/"
 rpmbuild -ba "${TOPDIR}/SPECS/${SPEC}"
