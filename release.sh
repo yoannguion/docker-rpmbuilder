f3ae361d0af1ba7c8b6b413cdc0ff355fa719cde
Yoann Guion <yguion@enovacom.fr>
2020-04-27 02:05:20 +0200

    echo before upload release

--- release.sh
+++ release.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+uploadToGitHub () {
+echo $1
+file=`basename $1`
+echo $file
+curl \
+  --fail \
+  -X POST \
+  --data-binary @$1 \
+  -H 'Content-Type: application/octet-stream' \
+  -H "Authorization: Bearer ${RELEASE_GITHUB_TOKEN}" \
+  "${RELEASE_ASSETS_UPLOAD_URL}?name=$file"
+return $?
+}
+
+
 cat ${GITHUB_EVENT_PATH}
 
 # prepare upload URL
@@ -19,19 +34,5 @@ for i in $(find /home/rpmbuild/rpmbuild/RPMS/x86_64/ -name *.rpm); do
   fi
 done
 
-function uploadToGitHub ()
-{
-echo $1
-file=`basename $1`
-echo $file
-curl \
-  --fail \
-  -X POST \
-  --data-binary @$1 \
-  -H 'Content-Type: application/octet-stream' \
-  -H "Authorization: Bearer ${RELEASE_GITHUB_TOKEN}" \
-  "${RELEASE_ASSETS_UPLOAD_URL}?name=$file"
-return $?
-}
 
 
