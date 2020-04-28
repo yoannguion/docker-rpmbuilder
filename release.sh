126865563cb6cae1c51d9c8de3f8f2b0653b684c
Yoann Guion <yguion@enovacom.fr>
2020-04-28 14:27:37 +0200

    use RELEASE_TAG variable or use latest

--- release.sh
+++ release.sh
@@ -15,11 +15,25 @@ return $?
 
 
 cat ${GITHUB_EVENT_PATH}
-# prepare upload URL
 REPO_URL=$(cat ${GITHUB_EVENT_PATH} | jq -r .repository.url)
 echo $REPO_URL
-LATEST_RELEASE_URL="$REPO_URL/releases/latest"
-echo $LATEST_RELEASE_URL
+# prepare upload URL
+if [ -z $RELEASE_TAG ]; then
+
+  LATEST_RELEASE_URL="$REPO_URL/releases/latest"
+else
+        nb_good_tab=`curl -H "Authorization: Bearer ${RELEASE_GITHUB_TOKEN}" "$REPO_URL/tags" | grep "$RELEASE_TAG" | wc -l`
+        if [ "$nb_good_tab" == "0" ]; then
+                echo "should create tag"
+                json="{\"tag_name\": \"$RELEASE_TAG\", \"name\": \"$RELEASE_TAG\"}"
+                echo "$json"
+                curl -X POST -H "Authorization: Bearer ${RELEASE_GITHUB_TOKEN}" -H "Content-Type: application/json" -d "$json" "$REPO_URL/releases"
+        else
+                echo "tag $RELEASE_TAG already exist"
+        fi
+    LATEST_RELEASE_URL="$REPO_URL/releases/tags/$RELEASE_TAG"
+fi
+echo $LATEST_RELEASE_URL;
 RELEASE_ASSETS_UPLOAD_URL=$(curl -H "Authorization: Bearer ${RELEASE_GITHUB_TOKEN}" $LATEST_RELEASE_URL | jq -r .upload_url)
 RELEASE_ASSETS_UPLOAD_URL=${RELEASE_ASSETS_UPLOAD_URL%\{?name,label\}}
 echo "release url $RELEASE_ASSETS_UPLOAD_URL"
